6ca017578c048f4009c19636e128686251349f8f
abubakar324 <abubakar.sheikhyunis@gmail.com>
2025-06-09 09:54:31 +0300

    arith lab

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+let num1= 31;
+let num2= 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+let num3 = 25;
+let num4 = 21;
+let mod = num3 % num4;
+
+let max = Math.max(5, 10, 15, 20);
+module.exports = {
+    num1,
+    num2,
+    multiply,
+    random,
+    num3,
+    num4,
+    mod,
+    max
+};
+    
